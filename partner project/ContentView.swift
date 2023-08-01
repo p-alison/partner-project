faa405cb276956e34ee84906f5fa738bcded6eb3
Gianna Booker <bookerfamily09@gmail.com>
2023-08-01 11:21:54 -0400

    Added text

--- partner project/ContentView.swift
+++ partner project/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Partners")
+            Text("Hello!")
         }
         .padding()
     }
