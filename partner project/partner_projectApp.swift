27ffca50bc4c730ba07fddab8bc7184c6e77af59
Alison <pintoalison87@gmail.com>
2023-08-01 11:06:55 -0400

    Initial Commit

---
+++ partner project/partner_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  partner_projectApp.swift
+//  partner project
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct partner_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
